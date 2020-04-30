9ecf6458b00e5297a64323580ee5275244276054
pablo <serranopablonavarro@gmail.com>
2020-04-30 07:50:09 -0700

    Commit, contentcreator delete and detail

--- src/app/content-creator/content-creator-delete/content-creator-delete.component.ts
+++ src/app/content-creator/content-creator-delete/content-creator-delete.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContentcreatorService } from '../contentcreator.service';
+import { Contentcreator } from '../contentcreator';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-content-creator-delete',
-  templateUrl: './content-creator-delete.component.html',
-  styleUrls: ['./content-creator-delete.component.css']
+  templateUrl: './content-creator-delete.component.html'
 })
 export class ContentCreatorDeleteComponent implements OnInit {
+  public user: Contentcreator = new Contentcreator();
+  private id: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private contentcreatorService: ContentcreatorService,
+              private authenticationService: AuthenticationBasicService) {
   }
 
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.contentcreatorService.get(this.id).subscribe(
+      contentcreator => this.user = contentcreator);
+  }
+
+  delete() {
+    this.contentcreatorService.delete(this.user).subscribe(
+      () => {
+        this.authenticationService.logout();
+        this.router.navigate(['']);
+      });
+  }
 }
--- src/app/content-creator/content-creator-detail/content-creator-detail.component.ts
+++ src/app/content-creator/content-creator-detail/content-creator-detail.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContentcreatorService } from '../contentcreator.service';
+import { Contentcreator } from '../contentcreator';
+import { User } from '../../login-basic/user';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-content-creator-detail',
-  templateUrl: './content-creator-detail.component.html',
-  styleUrls: ['./content-creator-detail.component.css']
+  templateUrl: './content-creator-detail.component.html'
 })
 export class ContentCreatorDetailComponent implements OnInit {
+  public contentcreator: Contentcreator = new Contentcreator();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private contentcreatorService: ContentcreatorService,
+              private authenticationService: AuthenticationBasicService) {
   }
 
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.contentcreatorService.get(id).subscribe(
+      contentcreator => {
+        this.contentcreator = contentcreator;
+      });
+  }
+
+  getCurrentUser(): User {
+    return this.authenticationService.getCurrentUser();
+  }
 }
