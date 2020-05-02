8101f87c0cb805fc4ff644109d28a75a5b3f066e
aidagorguesmanzanares <aida.gorgues@gmail.com>
2020-05-02 20:21:08 +0200

    ImageNameQuestion create

--- src/app/questions/image-name-questions/image-name-question-create/image-name-question-create.component.ts
+++ src/app/questions/image-name-questions/image-name-question-create/image-name-question-create.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ImageNameQuestion } from '../imageNameQuestion';
+import { Router } from '@angular/router';
+import { ImageNameQuestionService } from '../image-name-question.service';
+import { AuthenticationBasicService } from 'src/app/login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-image-name-question-create',
@@ -6,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageNameQuestionCreateComponent implements OnInit {
 
-  constructor() { }
+  public imageNameQuestion: ImageNameQuestion;
+
+  constructor(private router: Router,
+              private imageNameQuestionService: ImageNameQuestionService,
+              private authenticationService: AuthenticationBasicService) { }
 
   ngOnInit(): void {
+    this.imageNameQuestion = new ImageNameQuestion();
+  }
+
+  onSubmit(): void {
+    this.imageNameQuestionService.create(this.imageNameQuestion).subscribe(
+      (imageNameQuestion:ImageNameQuestion) => {
+        this.router.navigate([imageNameQuestion.uri]);
+      }
+    )
   }
 
 }
