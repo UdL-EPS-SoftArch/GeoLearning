95489bf6ff0740c612f12eefad7a13488d1b3f4f
ar98nau <anr3@alumnes.udl.cat>
2020-05-19 13:49:12 +0200

    is/isNot imageName Write

--- src/app/games/image-name/image-name-detail/image-name-detail.component.ts
+++ src/app/games/image-name/image-name-detail/image-name-detail.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ImageName } from '../imageName';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ImageNameService } from '../image-name.service';
 import { AuthenticationBasicService } from 'src/app/login-basic/authentication-basic.service';
+import { NgbCheckBox } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-image-name-detail',
   templateUrl: './image-name-detail.component.html'
 })
 export class ImageNameDetailComponent implements OnInit {
+
   public imageName: ImageName = new ImageName();
   public id: string;
 
   constructor(private route: ActivatedRoute,
-              private imageNameService: ImageNameService) { }
+              private imageNameService: ImageNameService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     this.imageNameService.get(this.id).subscribe(
       imageName => {
         this.imageName = imageName;
+      });
+  }
+
+  onCheckedBox(target) {
+    this.imageName.isWrite = target.checked;
+    this.imageNameService.update(this.imageName).subscribe(
+      () => {
+        this.router.navigate(['/imageNames/' + this.id]);
       });
   }
 
--- src/app/games/image-name/imageName.ts
+++ src/app/games/image-name/imageName.ts
@@ -3,6 +3,7 @@ import { ImageNameQuestion } from 'src/app/questions/image-name-questions/imageN
 
 export class ImageName extends Game {
     questions: ImageNameQuestion[] = [];
+    isWrite: Boolean;
 
     constructor(values: object = {}) {
         super();
